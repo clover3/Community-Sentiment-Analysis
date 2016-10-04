5c44838d715aff92193ae47e518a9a3304b0eb23
clover3 <lesterny@gmail.com>
2016-10-04 18:10:33 +0900

    Refactoring & Korean support

--- KerasClassifier/CNN.py
+++ KerasClassifier/CNN.py
@@ -1,3 +1,4 @@
+from clover_lib import *
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Convolution1D, Convolution2D, MaxPooling1D, MaxPooling2D, Embedding, Reshape
@@ -8,74 +9,194 @@ from keras.optimizers import SGD
 from keras.optimizers import Adadelta
 import cPickle
 import numpy
-
+import os
+import codecs, csv
 
 seed = 7
 numpy.random.seed(seed)
 
 len_sentence = 64
 
-def index_rize(text_list, k):
+
+def load_vec(fname, vocab, binary = True):
+    """
+    Loads 300x1 word vecs from Google (Mikolov) word2vec
+    """
+    print("  loading word2vec...")
+    w2v_cache = "cache\\w2v"
+    if os.path.isfile(w2v_cache):
+        return cPickle.load(open(w2v_cache,"rb"))
+
+    mode = (binary if "rb" else "r")
+    word_vecs = {}
+    with open(fname, mode) as f:
+        header = f.readline()
+        vocab_size, layer1_size = map(int, header.split())
+        binary_len = numpy.dtype('float32').itemsize * layer1_size
+
+        def getline():
+            if binary:
+                return numpy.fromstring(f.read(binary_len), dtype='float32')
+            else:
+                return numpy.array(f.readline().split(), dtype='float32')
+
+        for line in xrange(vocab_size):
+            word = []
+            while True:
+                ch = f.read(1)
+                if ch == ' ':
+                    word = ''.join(word)
+                    break
+                if ch != '\n':
+                    word.append(ch)
+            if word in vocab:
+                word_vecs[word] = getline()
+            else:
+                getline()
+
+    cPickle.dump(word_vecs, open(w2v_cache, "wb"))
+    return word_vecs
+
+
+def get_voca(text_list):
     voca = set()
-
-    max_l = 0
     for text in text_list:
-        tokens = text.split()
-        if len(tokens) > max_l :
-            max_l = len(tokens)
-        voca.update(tokens)
+        voca.update(text.split())
+    return voca
 
-    count = 1
-    voca_dict = dict()
+
+def build_index(voca, w2v, k):
+    index = 1
+    word2idx = dict()
+    idx2vect = numpy.zeros(shape=(len(voca) + 1, k), dtype='float32')
+
+    idx2vect[0] = numpy.zeros(k, dtype='float32')
+    if w2v is not None:
+        for word in w2v.keys():
+            word2idx[word] = index
+            idx2vect[index] = w2v[word]
+            index += 1
+
     for word in voca:
-        voca_dict[word]= count
-        count += 1
+        if word not in word2idx:
+            word2idx[word] = index
+            idx2vect[index] = numpy.zeros(k, dtype='float32')
+            index += 1
 
-    max_l = len_sentence
+    return word2idx, idx2vect
 
-    print "max_l" , max_l
-    vectors = []
-    for text in text_list:
-        vector = list(map(lambda x: voca_dict[x], text.split()))
-        while len(vector) < max_l :
-            vector.append(0)
-        vectors.append(numpy.array(vector, dtype='float32'))
-
-
-    nv = numpy.array(vectors)
-    return nv, len(voca_dict)
 
 class DataSet:
-    def __init__(self, train_x, train_y, test_x, test_y, voca_size, len_embedding):
+    def __init__(self, dataset_x, dataset_y, idx2vect, len_embedding):
+        data = zip(dataset_x, dataset_y)
+        numpy.random.shuffle(data)
+        len_train = int(len(data) * 0.9)
+        train_x, train_y = zip(*data[0:len_train])
+        test_x, test_y = zip(*data[len_train:])
+
         self.train_x = numpy.array(train_x)
         self.train_y = numpy.array(train_y)
         self.test_x = numpy.array(test_x)
         self.test_y = numpy.array(test_y)
-        self.voca_size = voca_size
+
+        self.idx2vect = idx2vect
         self.len_embedding = len_embedding
 
+def load_data_label(label_path, path_article, w2v_path, dimension):
+    IDX_R_ARTICLE = 1
+    IDX_R_KEYWORD= 2
+    IDX_R_LABEL = 3
 
-def load_data(pos_path, neg_path, dimension):
+    # load label table
+    labels = load_csv(label_path)
+    labels = [label for label in labels if label[IDX_R_LABEL] == 0 or label[IDX_R_LABEL] == 2 ]
+
+    # load article table
+    articles = parse_token(load_csv(path_article))
+    article_dic = dict()
+    for article in articles :
+        article_id = article[IDX_ARTICLE_ID]
+        article_dic[article_id] = article
+
+    voca = set(flatten([article[IDX_TOKENS] for article in articles]))
+
+    w2v = load_vec(w2v_path, voca, False)
+    word2idx, idx2vect = build_index(voca, w2v, dimension)
+
+    def indexize(label):
+        def find_keyword(article, keyword):
+            tokens = article[IDX_TOKENS]
+            indexs = [i for i,x in enumerate(tokens) if (keyword in x)]
+            if len(indexs) > 0:
+                return indexs[0]
+            # if keyword is mis-splitted, we need to find its location.
+            ### IT CONTAINS BUG! ###
+
+            scan = ""
+            for i, token in enumerate(tokens)[:-1]:
+                scan = token + scan
+                if keyword in scan:
+                    return i
+
+            raise "Keyword not found!!"
+        #  lookup the corpus to find specified article
+
+        article = article_dic[label[IDX_R_ARTICLE]]
+        keyword = label[IDX_R_KEYWORD]
+        idx_keyword = find_keyword(article, keyword)
+        pre_len = (len_sentence-1) / 2
+        post_len = len_sentence - pre_len - 1
+
+        # slice the content to specific size.
+        # If there isn't enough token before keyword, use all from beginning
+        if idx_keyword < pre_len :
+            begin = 0
+            end = len_sentence
+        else :
+            begin = idx_keyword - pre_len
+            end = idx_keyword - pre_len + len_sentence
+
+        # if lenght exceed, pad with zero
+        tokens = article[IDX_TOKENS]
+        result = []
+        for i in range(begin, end):
+            if i < len(tokens) :
+                result.append(word2idx[tokens[i]])
+            else :
+                result.append(0)
+        return result
+
+    data_set_x = [indexize(label) for label in labels]
+    data_set_y = [label[IDX_R_LABEL] for label in labels]
+    return DataSet(data_set_x, data_set_y, idx2vect, dimension)
+
+
+def load_data(pos_path, neg_path, w2v_path, dimension):
     print("Loading Data...")
     raw_pos = open(pos_path).readlines()
     raw_neg = open(neg_path).readlines()
+    raw_data = raw_pos+raw_neg
+    w2v = load_vec(w2v_path, get_voca(raw_data)) # w2v : word->float[]
 
-    dataset_x, voca_size = index_rize(raw_pos+raw_neg, dimension)
+    voca = get_voca(raw_data)
+    word2idx, idx2vect = build_index(voca, w2v, dimension)
+
+    # Convert text into index
+    vectors = []
+    for text in raw_data:
+        vector = list(map(lambda x: word2idx[x], text.split()))
+        while len(vector) < len_sentence :
+            vector.append(0)
+        vectors.append(numpy.array(vector, dtype='float32'))
+
+    dataset_x = numpy.array(vectors)
     dataset_y = len(raw_pos)*[1] + len(raw_neg) * [0]
+    return DataSet(dataset_x, dataset_y, idx2vect, dimension)
 
-    data = zip(dataset_x, dataset_y)
-    numpy.random.shuffle(data)
-    len_train = int(len(data) * 0.9)
-    train_x, train_y = zip(*data[0:len_train])
-    test_x, test_y = zip(*data[len_train:])
-
-    data_set = DataSet(train_x, train_y, test_x, test_y, voca_size, dimension)
-    return data_set
 
 
 def get_model(len_embedding, size_voca):
-    print("Creating model...")
-    len_sentence = 100
+    print("Creating Model : ")
     n_filter = 500
 
     model = Sequential()
@@ -103,28 +224,30 @@ def get_model(len_embedding, size_voca):
     return model
 
 
-def get_model_dirty(len_embedding, size_voca):
-    print("Creating model...")
+def get_model_dirty(len_embedding, data, filter_sizes):
+    print "Creating Model... ",
 
     dropout_prob = (0.5, 0.5)
     n_filter = 500
     hidden_dims = 100
+    size_voca = len(data.idx2vect)
     sent_input = Input(shape=(len_sentence,), dtype='int32', name='sent_level_input')
-    sent_x = Embedding(size_voca + 1, len_embedding,
-                       input_length=len_sentence)(sent_input)
+    print " Embedding Layer ",
+    sent_x = Embedding(size_voca, len_embedding,
+                       input_length=len_sentence, weights=[data.idx2vect])(sent_input)
     sent_x = Dropout(dropout_prob[0], input_shape=(len_sentence, len_embedding))(sent_x)
 
+    print " Convolution Layer ",
     multiple_filter_output= []
     convs = []
     pools = []
-    filter_sizes = (3, 4, 5)
-    for i in xrange(len(filter_sizes)):
+    for i, filter_size in enumerate(filter_sizes):
         convs.append(Convolution1D(nb_filter=n_filter,
-                         filter_length= filter_sizes[i],
+                         filter_length= filter_size,
                          border_mode='valid',
                          activation='relu',
                          subsample_length=1)(sent_x))
-        pools.append(MaxPooling1D(pool_length = len_sentence - filter_sizes[i] + 1)(convs[i]))
+        pools.append(MaxPooling1D(pool_length = len_sentence - filter_size + 1)(convs[i]))
         multiple_filter_output.append(Flatten()(pools[i]))
     sent_v = merge(multiple_filter_output, mode = 'concat')
 
@@ -138,21 +261,47 @@ def get_model_dirty(len_embedding, size_voca):
     model = Model(input=sent_input, output=sent_loss)
     #model.load_weights("YoonKim_mr.h5")
     model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer=adadelta)
+    print "  Done!"
     return model
 
-def run():
+def run_english():
     len_embedding = 300
 
-    data = load_data("rt-polarity-small.pos", "rt-polarity-small.neg", len_embedding)
+    path_prefix = "input\\rt-polarity."
+    pos_path = path_prefix + "pos"
+    neg_path = path_prefix + "neg"
+    w2v_path = "input\\GoogleNews-vectors-negative300.bin"
 
-    model = get_model_dirty(len_embedding, data.voca_size)
+    data = load_data(pos_path, neg_path, w2v_path, len_embedding)
+
+    model = get_model_dirty(len_embedding, data, [3, 4, 5])
     #model = get_model(len_embedding, data.voca_size)
 
     print("Training Model...")
     model.fit(data.train_x, data.train_y,
               batch_size=50,
-              nb_epoch=10,
+              nb_epoch=40,
               validation_data=(data.test_x, data.test_y),
               shuffle=True)
 
-run()
+def run_korean():
+    len_embedding = 300
+
+    path_lable = "input\\label.csv"
+    w2v_path = "input\\embedding300"
+    path_article = "input\\bobae....."
+
+    data = load_data_label(path_lable, path_article, w2v_path, len_embedding)
+
+    model = get_model_dirty(len_embedding, data, [3,4,5])
+    #model = get_model(len_embedding, data.voca_size)
+
+    print("Training Model...")
+    model.fit(data.train_x, data.train_y,
+              batch_size=50,
+              nb_epoch=40,
+              validation_data=(data.test_x, data.test_y),
+              shuffle=True)
+
+
+run_english()
---
+++ KerasClassifier/clover_lib.py
@@ -0,0 +1,106 @@
+# -*- coding: euc-kr -*-
+import csv
+import codecs
+import threading
+
+IDX_RATING = 0
+IDX_TITLE = 1
+IDX_CONTENT = 2
+IDX_AUTHOR_ID = 3
+IDX_ARTICLE_ID = 5
+IDX_THREAD_ID = 6
+IDX_TEXT_TYPE = 9
+IDX_TOKENS = 10
+
+
+def load_csv(path):
+    with codecs.open(path, "rb", "utf-8") as f:
+        return [line for line in csv.reader(f)]
+
+
+def load_csv_euc_kr(path):
+    with codecs.open(path, "rb", "cp949") as f:
+        return [line for line in csv.reader(f)]
+
+
+def save_csv_euc_kr(data, path):
+    with codecs.open(path, "wb", 'cp949') as f:
+        csv.writer(f).writerows(data)
+
+def save_csv_utf(data, path):
+    with codecs.open(path, "wb", 'utf-8') as f:
+        csv.writer(f).writerows(data)
+
+def load_list(path):
+    with codecs.open(path, "r", 'utf-8') as f:
+        list = f.read().splitlines()
+        return list
+
+
+# if contain any keyword, return the keyword. else, return null string
+def contain_any(text, iterable):
+    for keyword in iterable:
+        if keyword in text:
+            return keyword
+    return ""
+
+
+def parse_token(articles):
+    result = []
+    for article in articles:
+        try:
+            tokens = article[IDX_TOKENS].split('/')
+            n_article = article[0:IDX_TOKENS] + [tokens]
+            result.append(n_article)
+        except Exception as e:
+            print(e)
+    return result
+
+
+class CaseCounter:
+    def __init__(self):
+        self.dic = {}
+
+    def add_count(self, id):
+        if id in self.dic:
+            count = self.dic[id]
+            self.dic[id] = count + 1
+        else:
+            self.dic[id] = 1
+
+    def enum_count(self):
+        for key in self.dic.keys():
+            print("{} : {}".format(key, self.dic[key]))
+
+
+class FailCounter:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.success = 0
+        self.failure = 0
+
+    def total(self):
+        return self.success + self.failure
+
+    def suc(self):
+        self.lock.acquire()
+        self.success += 1
+        self.lock.release()
+
+    def fail(self):
+        self.lock.acquire()
+        self.failure += 1
+        self.lock.release()
+
+    def add_fail(self, count):
+        self.lock.acquire()
+        self.failure += count
+        self.lock.release()
+
+    def precision(self):
+        total = self.success + self.failure
+        return float(self.success) / total
+
+
+def flatten(z):
+    return [y for x in z for y in x]
