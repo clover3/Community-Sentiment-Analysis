e4f641019db7ba3bd92b6f8f9d7b9ddf12f110c6
clover3 <lesterny@gmail.com>
2016-12-29 18:16:02 +0900

    fixes #2

--- scala/src/main/scala/category.scala
+++ scala/src/main/scala/category.scala
@@ -7,46 +7,103 @@ import com.github.tototoshi.csv._
 
 object category{
 
-  trait Tag {
-    val parent : Tag
-    val name : String
+  type WordID = Int
+  type SynsetIdx = Int
+  type SynsetOffset = Int
+  type StdIdx = Int
+  class Tag(id : StdIdx, parentID:List[SynsetOffset], val name:String){
   }
 
-  trait Category {
-    val tags : List[Tag]
+  type Category = List[Tag]
+
+  def printCategory(c : Category) = {
+    c foreach ( x => print(x.name + ",") )
   }
 
   def loadCSV(path :String) : Stream[List[String]] = {
-    val reader = CSVReader.open(new File(path))
+    val reader = CSVReader.open(new File(path), "euc-kr")
     val stream : Stream[List[String]] = reader.toStream
     stream
   }
 
-  type WordID = Int
-  type TagID = Int
-  type Hypernym = TagID
 
-  def loadCategory(pathStdDic: String, pathSynset:String, pathTran:String) : Map[String, Category] = {
+  // StdDic : Standard Dictionary : Human words
+  // Synset : Symentic Set : Categorical information
+  // tran : Translation table
+  def loadCategory(pathStdDic: String, pathSynset:String, pathTran:String) : List[(String, Category)] = {
     def parseStdDic( l : List[String]) : (Int, String) = {
       (l.head.toInt, l.tail.head)
     }
-    val stringTokens : Stream[(Int, String)] = loadCSV(pathStdDic) map parseStdDic
-    val stdDic : Map[Int,String] = stringTokens.toList.toMap
+    val stdDic : Stream[(StdIdx, String)] = loadCSV(pathStdDic) map parseStdDic
+    val stdDicMap : Map[StdIdx,String] = stdDic.toList.toMap
 
 
-    def parseSynset( l : List[String]) : (TagID, String, Array[Hypernym]) = {
-      def parseHypernym(str : String) : Hypernym = str.split("_")(0).toInt
+    def parseSynset( l : List[String]) : (StdIdx, SynsetOffset, String, List[SynsetOffset]) = {
+      def parseHypernym(str : String) : SynsetOffset = str.split("_")(0).toInt
 
       val arr = l.toArray
+      val synsetidx = arr(0).toInt
       val offset = arr(1).toInt
-      val word : String = arr(2)
-      val hypernymStrs : Array[Hypernym] = arr(4).split(",") map parseHypernym
+      val word: String = arr(2)
+      val hypernymStrs : List[SynsetOffset] = {
+        if(arr(4).isEmpty)
+          Nil
+        else
+          (arr(4).split(",") map parseHypernym).toList
+      }
 
-      (offset, word, hypernymStrs)
+      (synsetidx, offset, word, hypernymStrs)
     }
-    val synset : Stream[(TagID, String, Array[Hypernym]) ] = loadCSV(pathStdDic) map parseSynset
+    val synset : Stream[(SynsetIdx, SynsetOffset, String, List[SynsetOffset]) ] = loadCSV(pathSynset) map parseSynset
+    val parentsOf : Map[SynsetOffset, List[SynsetOffset]] = (synset.toList map (x => (x._2, x._4) )).toMap
+    val synsetIdx2offset : Map[SynsetIdx, SynsetOffset] = (synset.toList map (x => (x._1, x._2) )).toMap
+    val nameOf : Map[SynsetOffset, String] = (synset.toList map (x => (x._2, x._3) )).toMap
 
-    val tran : Stream[(TagID, String, Array[Hypernym]) ] = loadCSV(pathTran) map parseSynset
+    def parseTran( l : List[String]) : (Int, SynsetIdx, StdIdx, String) = {
+      val arr = l.toArray
+      val tranidx = arr(0).toInt
+      val synsetidx = arr(1).toInt
+      val stdDicidx = arr(3).toInt
+      val word : String = arr(6)
+      (tranidx, synsetidx, stdDicidx, word)
+    }
+    val tran : Stream[(Int, SynsetIdx, StdIdx, String) ] = loadCSV(pathTran) map parseTran
+    val synsetOffset2Name : Map[SynsetOffset, String] = (tran map (x => (synsetIdx2offset(x._2), x._4))).toMap
+    val stdIdx2SynsetOffset : Map[StdIdx, SynsetOffset] = (tran map (x => (x._3, synsetIdx2offset(x._2)))).toMap
+    // for every string in stdDic
+    // find matching stddicIdx in trans
+    // travel along wordnetidx to extract all word set
+    // add all tag set to
+    def getNameStr(synsetOffset: SynsetOffset) : String = {
+      val synsetStr = nameOf(synsetOffset)
+      val transName = {
+        if(synsetOffset2Name.contains(synsetOffset))
+          synsetOffset2Name(synsetOffset)
+        else
+          "-"
+      }
+      synsetStr + " " + transName
+    }
+    def allTagsFor(stdDicEntry : (StdIdx, String)) : Category = {
+      val stdidx = stdDicEntry._1
+      val synsetOffset : SynsetOffset = stdIdx2SynsetOffset(stdidx)
+
+      def follow(first : List[SynsetOffset]) : List[SynsetOffset] = {
+        if (first.isEmpty)
+          Nil
+        else {
+          val parents: List[SynsetOffset] = (first map parentsOf).flatten
+          first ++ follow(parents)
+        }
+      }
+
+      val tagsIDs : List[SynsetOffset] = follow(List(synsetOffset))
+      tagsIDs map (x => new Tag(x, parentsOf(x), getNameStr(x) ) )
+    }
+
+    val validStdDIc = stdDic filter (x => stdIdx2SynsetOffset.contains(x._1))
+    val strm = validStdDIc map ( x=> (x._2, allTagsFor(x)))
+    strm.toList
   }
 
 }
--- scala/src/test/scala/categoryTest.scala
+++ scala/src/test/scala/categoryTest.scala
@@ -5,6 +5,16 @@ import category._
 class CategorySuite extends FunSuite {
 
   test("check category load"){
-    loadCategory("data\\StdDic.txt", "data\\Synset.txt", "data\\Tran.txt")
+    val categories = loadCategory("data\\StdDic.txt", "data\\Synset.txt", "data\\Tran.txt")
+
+    def printer(entry : (String, Category)) = {
+      print(entry._1)
+      print(":")
+      printCategory(entry._2)
+      println("")
+    }
+    val mother = categories filter ( x=> x._1 contains "차")
+    mother foreach printer
+
   }
 }
